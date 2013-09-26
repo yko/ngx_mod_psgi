3655b4306b0e145d0217706b2dc4b10c577a4370
yko <ykorshak@gmail.com>
2013-09-26 22:46:51 +0300

    cleanup

--- src/perlxsi.c
+++ src/perlxsi.c
@@ -8,9 +8,8 @@ EXTERN_C void boot_DynaLoader (pTHX_ CV* cv);
 EXTERN_C void
 xs_init(pTHX)
 {
-	char *file = __FILE__;
-	dXSUB_SYS;
+    char *file = __FILE__;
+    dXSUB_SYS;
 
-	/* DynaLoader is a special case */
-	newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
+    newXS("DynaLoader::boot_DynaLoader", boot_DynaLoader, file);
 }
